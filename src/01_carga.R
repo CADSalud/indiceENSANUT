35f761aa745ec74886b9c302b03c434cb5de2dbd
David Lampon <jdlampon@gmail.com>
2016-09-13 10:29:22 -0500

    Carga de archivos SPSS a R

---
+++ src/01_carga.R
@@ -0,0 +1,9 @@
+setwd("~/Documents/cad/ensanut2012/salud/")
+
+library(foreign)
+
+adultos <- read.spss('Adultos.sav')
+
+etiquetas <- attributes(adultos)$variable.labels
+
+adultos.df <- as.data.frame(adultos)
